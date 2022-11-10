228f7aaf393d7d4b44cffacd541c1e136c8a91a5
Dimokus <Dimokus@mail.ru>
2022-11-11 00:45:50 +0300

    Update main.sh

--- Active_testnets/okp4/main.sh
+++ Active_testnets/okp4/main.sh
@@ -124,7 +124,11 @@ rm -rf $HOME/okp4d/data
 rm -rf $HOME/okp4d/wasm
 
 SNAP_NAME=$(curl -s https://snapshots2-testnet.nodejumper.io/okp4-testnet/ | egrep -o ">okp4-nemeton.*\.tar.lz4" | tr -d ">")
-
+echo ===============================================
+echo == Ожидайте скачивания снепшота до 2х часов. ==
+echo ===============================================
+echo = Expect the snapshot download up to 2 hours. =
+echo ===============================================
 curl https://snapshots2-testnet.nodejumper.io/okp4-testnet/${SNAP_NAME} | lz4 -dc - | tar -xf - -C $HOME/okp4d
 mv $HOME/okp4d/priv_validator_state.json.backup $HOME/okp4d/data/priv_validator_state.json
 
