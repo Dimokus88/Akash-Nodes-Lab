b93b9f63fbbaa25fb0735cf03d0c44ff4a1c45da
Dimokus <Dimokus@mail.ru>
2022-11-11 15:43:52 +0300

    Update snap.sh

--- Active_testnets/okp4/snap.sh
+++ Active_testnets/okp4/snap.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 source ~/.bashrc
-sv stop $BINARY
-sed -i.bak -E "s|^(enable[[:space:]]+=[[:space:]]+).*$|\1false|" /root/$BINARY/config/config.toml
+sv stop okp4d
+sleep 2
+sed -i.bak -E "s|^(enable[[:space:]]+=[[:space:]]+).*$|\1false|" /root/okp4d/config/config.toml
 apt install lz4 -y
-cp $HOME/okp4d/data/priv_validator_state.json $HOME/okp4d/priv_validator_state.json.backup
-cp $HOME/okp4d/data/priv_validator_key.json $HOME/okp4d/priv_validator_key.json.backup
-okp4d tendermint unsafe-reset-all --home $HOME/okp4d --keep-addr-book
-
-rm -rf $HOME/okp4d/data 
-rm -rf $HOME/okp4d/wasm
+sleep 2
+cp /root/okp4d/data/priv_validator_state.json /root/okp4d/priv_validator_state.json.backup
+cp /root/okp4d/data/priv_validator_key.json /root/okp4d/priv_validator_key.json.backup
+sleep 2
+okp4d tendermint unsafe-reset-all --home /root/okp4d --keep-addr-book
+sleep 2
+rm -rf /root/okp4d/data 
+rm -rf /root/okp4d/wasm
 
 SNAP_NAME=$(curl -s https://snapshots2-testnet.nodejumper.io/okp4-testnet/ | egrep -o ">okp4-nemeton.*\.tar.lz4" | tr -d ">")
 echo ===============================================
@@ -16,12 +19,12 @@ echo == Ожидайте скачивания снепшота до 2х часо
 echo ===============================================
 echo = Expect the snapshot download up to 2 hours. =
 echo ===============================================
-curl https://snapshots2-testnet.nodejumper.io/okp4-testnet/${SNAP_NAME} | lz4 -dc - | tar -xf - -C $HOME/okp4d
-mv $HOME/okp4d/priv_validator_state.json.backup $HOME/okp4d/data/priv_validator_state.json
-mv $HOME/okp4d/priv_validator_key.json.backup $HOME/okp4d/data/priv_validator_key.json
+curl https://snapshots2-testnet.nodejumper.io/okp4-testnet/${SNAP_NAME} | lz4 -dc - | tar -xf - -C /root/okp4d
+mv /root/okp4d/priv_validator_state.json.backup /root/okp4d/data/priv_validator_state.json
+mv /root/okp4d/priv_validator_key.json.backup /root/okp4d/data/priv_validator_key.json
 echo =======================
 echo == Снепшот загружен! ==
 echo =======================
 echo =Snapshot is uploaded!=
 echo =======================
-sv start $BINARY
+sv start okp4d
