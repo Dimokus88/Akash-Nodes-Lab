bf31df16018be757776e1cd511846324804ed58c
Dimokus <Dimokus@mail.ru>
2022-11-14 14:03:27 +0300

    Update main.sh

--- Active_testnets/Avalanche/main.sh
+++ Active_testnets/Avalanche/main.sh
@@ -2,12 +2,15 @@
 wget $BINARY
 archive=`ls` && tar -xf $archive
 rm $archive
+sleep 2
 cp `ls`/avalanchego /usr/bin/avax && cp `ls`/plugins /usr/bin/
 
 mkdir -p ./.avalanchego/configs/chains/P/
+sleep 2
 cat > ./.avalanchego/configs/chains/P/config.json <<EOF
 {
   "state-sync-enabled": true
 }
 EOF
+sleep 2
 avax
