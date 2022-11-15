f767544203fcb6c9c076d03eb1c91ee8de329f22
Dimokus <Dimokus@mail.ru>
2022-11-15 17:52:16 +0300

    Update main.sh

--- Active_testnets/Exorde/main.sh
+++ Active_testnets/Exorde/main.sh
@@ -35,31 +35,34 @@ fi
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 echo 'export YOUR_WALLET_ADDRESS='${YOUR_WALLET_ADDRESS} >> /root/.bashrc
 cd /
-conda init bash
+wget -O /anaconda.sh "https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh"
+chmod +x ./anaconda.sh
+(echo -e \n ; echo yes ; echo -e \n ; echo yes; echo yes) | ./anaconda.sh
+rm ./anaconda.sh
+PATH=$PATH:/n/bin
 source ~/.bashrc
 conda create --name exorde-env python=3.9 -y
 conda activate exorde-env
 pip install --upgrade pip
 git clone https://github.com/exorde-labs/ExordeModuleCLI.git
 cd ExordeModuleCLI
-PATH=$PATH:/ExordeModuleCLI
 pip install -r requirements.txt
 
-mkdir -p /root/Exorde/log    
-cat > /root/Exorde/run <<EOF 
+mkdir /ExordeModuleCLI/log    
+cat > /ExordeModuleCLI/run <<EOF 
 #!/bin/bash
 exec 2>&1
 exec python3 Launcher.py -m $YOUR_WALLET_ADDRESS -l 2
 EOF
-chmod +x /root/Exorde/run
+chmod +x /ExordeModuleCLI/run
 LOG=/var/log/Exorde
-cat > /root/Exorde/log/run <<EOF 
+cat > /ExordeModuleCLI/log/run <<EOF 
 #!/bin/bash
 mkdir $LOG
 exec svlogd -tt $LOG
 EOF
-chmod +x /root/Exorde/log/run
-ln -s /root/Exorde /etc/service
+chmod +x /ExordeModuleCLI/log/run
+ln -s /ExordeModuleCLI /etc/service
 ln -s /var/log/Exorde/current /LOG
 sleep 10
 tail -f /LOG
