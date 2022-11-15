57d7bd1854367426661235912a6caee59ff83b2e
Dimokus <Dimokus@mail.ru>
2022-11-15 17:48:13 +0300

    Update main.sh

--- Active_testnets/Exorde/main.sh
+++ Active_testnets/Exorde/main.sh
@@ -35,17 +35,14 @@ fi
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 echo 'export YOUR_WALLET_ADDRESS='${YOUR_WALLET_ADDRESS} >> /root/.bashrc
 cd /
-wget -O /anaconda.sh "https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh"
-chmod +x ./anaconda.sh
-(echo -e \n ; echo yes ; echo -e \n ; echo yes; echo yes) | ./anaconda.sh
-rm ./anaconda.sh
-PATH=$PATH:/n/bin
+conda init bash
 source ~/.bashrc
 conda create --name exorde-env python=3.9 -y
 conda activate exorde-env
 pip install --upgrade pip
 git clone https://github.com/exorde-labs/ExordeModuleCLI.git
 cd ExordeModuleCLI
+PATH=$PATH:/ExordeModuleCLI
 pip install -r requirements.txt
 
 mkdir -p /root/Exorde/log    
