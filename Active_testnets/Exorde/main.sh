89c8755f87b19fbc6ca68c51f8d3d246adedeefc
Dimokus <Dimokus@mail.ru>
2022-11-15 17:33:37 +0300

    Update main.sh

--- Active_testnets/Exorde/main.sh
+++ Active_testnets/Exorde/main.sh
@@ -1,4 +1,38 @@
 #!/bin/bash
+# By Dimokus (https://t.me/Dimokus)
+runsvdir -P /etc/service &
+cp /usr/lib/go-1.18/bin/go /usr/bin/
+# ++++++++++++ Установка удаленного доступа ++++++++++++++
+echo 'export MY_ROOT_PASSWORD='${MY_ROOT_PASSWORD} >> /root/.bashrc
+apt -y install tmate
+mkdir /root/tmate && mkdir /root/tmate/log
+cat > /root/tmate/run <<EOF 
+#!/bin/bash
+exec 2>&1
+exec tmate -F
+EOF
+cat > /root/tmate/log/run <<EOF 
+#!/bin/bash
+mkdir /var/log/tmate
+exec svlogd -tt /var/log/tmate
+EOF
+chmod +x /root/tmate/run
+chmod +x /root/tmate/log/run
+ln -s /root/tmate /etc/service
+
+if [[ -n $MY_ROOT_PASSWORD ]]
+then
+  echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+  (echo ${MY_ROOT_PASSWORD}; echo ${MY_ROOT_PASSWORD}) | passwd root && service ssh restart
+else
+  apt install -y goxkcdpwgen 
+  MY_ROOT_PASSWORD=$(goxkcdpwgen -n 1)
+  echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+  (echo ${MY_ROOT_PASSWORD}; echo ${MY_ROOT_PASSWORD}) | passwd root && service ssh restart
+  echo ============= SSH PASS: $MY_ROOT_PASSWORD ==============
+  sleep 10
+fi
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 echo 'export YOUR_WALLET_ADDRESS='${YOUR_WALLET_ADDRESS} >> /root/.bashrc
 cd /
 wget -O /anaconda.sh "https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh"
