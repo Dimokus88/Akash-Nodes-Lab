0197c3678e5053157fe76d6d2b002bc8e50ba09e
Dimokus <Dimokus@mail.ru>
2022-11-21 11:37:01 +0300

    Delete main.sh

--- Active_testnets/Ethereum2/main.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-apt-get install software-properties-common -y
-add-apt-repository -y ppa:ethereum/ethereum
-apt-get update
-apt-get install ethereum -y
-apt-get upgrade geth -y
-runsvdir -P /etc/service
-
