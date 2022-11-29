24ebb84bc91901044e226ffc87ca9dd658754ca9
Dimokus <Dimokus@mail.ru>
2022-11-29 23:37:39 +0300

    Update main.sh

--- Active_testnets/Kadena/main.sh
+++ Active_testnets/Kadena/main.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 if [[ -n $IP ]]
 then
-#runsvdir -P /etc/service &
-#apt-get update && apt-get upgrade -y
-#apt install runit -y
-echo =========== ip адрес найден =============
+runsvdir -P /etc/service &
+apt-get update && apt-get upgrade -y
+apt install runit -y
+
 sleep infinity
 else
 echo ip адрес не найден!
+echo Укажите во вкладке UPDATE выданный провайдером IP адрес (вкладка lease) и обновите деплоймент!
+echo Пример:
+echo ==================================
+echo =  ...                           =
+echo = services:                      =
+echo =  node:                         =
+echo =    image: kadena/chainweb-node =
+echo =    env:                        =
+echo =      - "IP=123.123.123.123     =
+echo =  ...                           =
+echo ==================================
 sleep infinity
 fi
