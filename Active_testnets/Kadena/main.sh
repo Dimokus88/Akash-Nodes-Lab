1b3178715685094538b92ecce9c131a7a49efa50
Dimokus <Dimokus@mail.ru>
2022-11-29 23:09:04 +0300

    Update main.sh

--- Active_testnets/Kadena/main.sh
+++ Active_testnets/Kadena/main.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-if [ -e /root/ipaddress ]
+if [ ! -s /root/ipaddress ]
 then
+echo ip адрес не найден!
+sleep infinity
+else
 #runsvdir -P /etc/service &
 #apt-get update && apt-get upgrade -y
 #apt install runit -y
 echo =========== ip адрес найден =============
-sleep infinity
-else
-echo ip адрес не найден!
 sleep infinity
 fi
