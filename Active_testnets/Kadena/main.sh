2a1f4402a96757d4c0c92a2f24bb51362c24e9ee
Dimokus <Dimokus@mail.ru>
2022-11-30 00:19:08 +0300

    Update main.sh

--- Active_testnets/Kadena/main.sh
+++ Active_testnets/Kadena/main.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 if [[ -n $IP ]]
 then
-/chainweb/chainweb-node --p2p-hostname `echo $IP`
+apt install runit -y
+runsvdir -P /etc/service &
+
+mkdir -p /root/kadena/log
+cat > /root/kadena/run <<EOF 
+#!/bin/bash
+exec 2>&1
+exec /chainweb/chainweb-node --p2p-hostname $IP
+EOF
+cat > /root/kadena/log/run <<EOF 
+#!/bin/bash
+mkdir /var/log/kadena
+exec svlogd -tt /var/log/kadena
+EOF
+chmod +x /root/kadena/run
+chmod +x /root/kadena/log/run
+
+ln -s /root/kadena /etc/service
+sleep 2
+ln -s /var/log/kadena/current /log
 else
 echo ip адрес не найден!
 echo Укажите во вкладке UPDATE выданный провайдером IP адрес указанный во вкладке lease и обновите деплоймент!
