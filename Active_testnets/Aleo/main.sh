1a047362a83c774e4f383c875ee2b1829fbbc8d4
Dimokus <Dimokus@mail.ru>
2022-12-10 18:39:50 +0300

    Create main.sh

---
+++ Active_testnets/Aleo/main.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+cd /
+runsvdir -P /etc/service &
+apt update
+git clone https://github.com/AleoHQ/snarkOS.git --depth 1 && cd snarkOS
+apt-get install build-essential curl clang gcc libssl-dev llvm make pkg-config tmux xz-utils tmate -y
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y && source $HOME/.cargo/env
+cargo install --path .
+
+#=====client======
+mkdir -p /root/client/log
+cat > /root/client/run <<EOF 
+#!/bin/bash
+exec 2>&1
+exec $(which snarkos) start --nodisplay --client `echo $PRIVAT_CODE | base64 -d`
+EOF
+cat > /root/client/log/run <<EOF 
+#!/bin/bash
+mkdir /var/log/client
+exec svlogd -tt /var/log/client
+EOF
+chmod +x /root/client/run
+chmod +x /root/client/log/run
+ln -s /root/client /etc/service
+#=================
+echo == Client is running ==
+echo == LOG: tail-f /var/log/client/current ==
+sleep 5
+echo === Instal prover...===
+sleep 1m
+#=====prover======
+mkdir /root/prover && mkdir /root/prover/log
+cat > /root/prover/run <<EOF 
+#!/bin/bash
+exec 2>&1
+exec $(which snarkos) start --nodisplay --prover `echo $PRIVAT_CODE | base64 -d`
+EOF
+cat > /root/prover/log/run <<EOF 
+#!/bin/bash
+mkdir /var/log/prover
+exec svlogd -tt /var/log/prover
+EOF
+chmod +x /root/prover/run
+chmod +x /root/prover/log/run
+ln -s /root/prover /etc/service
+#=================
+echo == Prover is running ==
+echo == LOG: tail-f /var/log/prover/current ==
+tmate -F
+sleep infinity
