a216585c2f79eb623384da1bf16fc31f014c469d
Dimokus <Dimokus@mail.ru>
2022-12-12 15:50:36 +0300

    Update main.sh

--- Active_testnets/Gitopia/main.sh
+++ Active_testnets/Gitopia/main.sh
@@ -106,7 +106,7 @@ then
   p=0
   count=0
   echo "Search peers..."
-  while [[ "$p" -le  "$n_peers" ]] && [[ "$count" -le 9 ]]
+  while [[ "$p" -le  "$n_peers" ]] && [[ "$count" -le 4 ]]
   do
 	  PEER=`curl -s  $SNAP_RPC/net_info? | jq -r .result.peers["$p"].node_info.listen_addr`
     if [[ ! "$PEER" =~ "tcp" ]] 
