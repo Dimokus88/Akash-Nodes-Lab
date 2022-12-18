aa5df1a02d948e8c579ccfafaa1fdbf5fd2c4492
Dimokus <Dimokus@mail.ru>
2022-12-18 17:52:13 +0300

    Update main.sh

--- Mainnets/Near/main.sh
+++ Mainnets/Near/main.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 apt update && apt upgrade -y
-apt install sudo nano clang make tmate -y
+apt install sudo nano clang make tmate wget curl tar ssh runit -y
+apt install -y git binutils-dev libcurl4-openssl-dev zlib1g-dev libdw-dev libiberty-dev cmake gcc g++ python protobuf-compiler libssl-dev pkg-config llvm awscli
+echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+(echo $PASS; echo $PASS) | passwd root
+service ssh start
 runsvdir -P /etc/service &
 curl "https://sh.rustup.rs" -sSf | sh -s -- -y
 source $HOME/.cargo/env
