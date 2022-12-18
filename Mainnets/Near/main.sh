6832c2d5f57770184d16d59c399b85ac77443639
Dimokus <Dimokus@mail.ru>
2022-12-18 19:15:23 +0300

    Update main.sh

--- Mainnets/Near/main.sh
+++ Mainnets/Near/main.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-apt update && apt upgrade -y
 apt install sudo nano clang make tmate wget tar ssh runit -y
 apt install -y git binutils-dev libcurl4-openssl-dev zlib1g-dev libdw-dev libiberty-dev cmake gcc g++ python protobuf-compiler libssl-dev pkg-config llvm awscli
 echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
@@ -10,8 +9,32 @@ curl "https://sh.rustup.rs" -sSf | sh -s -- -y
 source $HOME/.cargo/env
 rustup update stable
 source $HOME/.cargo/env
+echo == Клонирую и устанавливаю nearcore ===
+sleep 10
 git clone https://github.com/near/nearcore && cd nearcore
 git fetch origin --tags
-git checkout tags/$VERSION_NEARCORE -b mynode
+git checkout $VERSION_NEARCORE -b mynode
 make neard
-./target/release/neard --home ~/.near init --chain-id $CHAIN --download-genesis --download-config
+echo == сборка завершена ==
+sleep 10
+if [[ $CHAIN == localnet ]]
+then
+echo == Выбран localnet ==
+sleep 10
+cargo build --package neard --features nightly_protocol,nightly_protocol_features --release
+./target/release/neard --home ~/.near init --chain-id localnet
+./target/release/neard --home ~/.near run
+sleep infinity
+fi
+
+./target/release/neard --home ~/.near init --chain-id $CHAIN --download-genesis --download-config --boot-nodes $BOOT_NODES
+rm ~/.near/config.json
+wget -O ~/.near/config.json $CONFIG
+echo == Синхронизация заголовков ==
+sleep 10
+aws s3 --no-sign-request cp s3://near-protocol-public/backups/$CHAIN/rpc/latest .
+LATEST=$(cat latest)
+aws s3 --no-sign-request cp --no-sign-request --recursive s3://near-protocol-public/backups/$CHAIN/rpc/$LATEST ~/.near/data
+echo == запуск near ==
+sleep 10
+./target/release/neard --home ~/.near run
