f6879846ba1c0ca74e410132a8f9c403fa9a5169
Dimokus <Dimokus@mail.ru>
2023-01-24 17:28:29 +0300

    Update main.sh

--- Active_testnets/Lamina1/main.sh
+++ Active_testnets/Lamina1/main.sh
@@ -12,6 +12,11 @@ cd lamina1
 mkdir -p /lamina1/configs/testnet/
 curl -s https://raw.githubusercontent.com/Dimokus88/Akash-Nodes-Lab/main/Active_testnets/Lamina1/default.json >/lamina1/configs/testnet/default.json
 curl -s https://raw.githubusercontent.com/Dimokus88/Akash-Nodes-Lab/main/Active_testnets/Lamina1/genesis_combined.json > /lamina1/configs/testnet/genesis_combined.json
+cat > /lamina1/get_my_nodeid.sh <<EOF 
+#!/bin/sh
+curl -s -X POST --data '{"jsonrpc":"2.0","id":1,"method" :"info.getNodeID"}' -H 'content-type:application/json;' 127.0.0.1:9650/ext/info | cut -d '"' -f 10
+EOF
+
 mkdir -p /root/lamina1/log
 cat > /root/lamina1/run <<EOF 
 #!/bin/bash
@@ -27,5 +32,5 @@ chmod +x /root/lamina1/run
 chmod +x /root/lamina1/log/run
 ln -s /root/lamina1 /etc/service
 
-
+echo == YOUR NODE ID:  `/lamina1/get_my_nodeid.sh | grep NodeID`  ==
 sleep infinity
