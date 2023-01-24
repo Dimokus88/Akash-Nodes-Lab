870171e5defc4f15a925bcf82e937e3daf7c075e
Dimokus <Dimokus@mail.ru>
2023-01-24 16:05:53 +0300

    Update main.sh

--- Active_testnets/Lamina1/main.sh
+++ Active_testnets/Lamina1/main.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 TZ=Europe/Kiev
 ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
-apt install -y ssh runit tar wget nano golang
+apt-get install -y  ssh wget curl tar
+echo "PermitRootLogin yes" >> /etc/ssh/sshd_config;
+(echo $SSH_PASS; echo $SSH_PASS) | passwd root
+service ssh start
 wget $BINARY_LINK
 tar -xvzf lamina1.latest.ubuntu-latest.tar.gz
 cd lamina1
+curl https://lamina1.github.io/lamina1/config.testnet.tar | tar xf -
+./lamina1-node  --config-file configs/testnet/default.json
+sleep infinity
